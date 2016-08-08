f96fc79ef984c913289bd3ca0cebc742f79a8cd2
WushuputiGH <lanou@Mac-Mini-5.local>
2016-08-08 21:29:47 +0800

    根据openGL, 自己做了一个全景展示的demo, 支持手势以及陀螺仪

---
+++ 全景_图片/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  全景_图片
+//
+//  Created by lanou on 16/8/4.
+//  Copyright © 2016年 H. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <GLKit/GLKit.h>
+
+@interface ViewController : GLKViewController
+
+
+@end
+
